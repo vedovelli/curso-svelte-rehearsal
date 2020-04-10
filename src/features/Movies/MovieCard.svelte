7fff453d7c103c4340d046918b2ef9a7ee32cd0b
Fabio Vedovelli <vedovelli@gmail.com>
2020-04-10 11:00:21 +0200

    Implements mobile first responsive design

--- src/features/Movies/MovieCard.svelte
+++ src/features/Movies/MovieCard.svelte
@@ -14,18 +14,20 @@
 </script>
 
 <div class="p-4">
-  <div class="flex">
+  <div class="flex flex-col md:flex-row">
     <div class="flex-shrink-0">
       <img
-        class="w-56 rounded"
+        loading="lazy"
+        class="rounded-lg md:w-56 md:rounded"
         src={`${BACKDROP_BASE_URL}${backdrop_path}`}
         alt="Woman paying for a purchase" />
     </div>
-    <div class="ml-6 w-full">
-      <div class="text-sm font-bold tracking-wide text-red-600 uppercase">
+    <div class="w-full md:ml-6">
+      <div
+        class="mt-3 text-sm font-bold tracking-wide text-red-600 uppercase md:mt-0">
         {title}
       </div>
-      <div class="flex text-xs text-gray-500 bg-gray-100 my-2 p-2 rounded">
+      <div class="flex p-2 my-2 text-xs text-gray-500 bg-gray-100 rounded">
         <p class="w-full">
           Avaliação:
           <strong>{vote_average}</strong>
@@ -35,7 +37,7 @@
           Lançamento: {new Date(release_date).toLocaleDateString()}
         </p>
       </div>
-      <p class="h-20 mt-2 text-sm text-gray-500">{overview.substr(0, 280)}</p>
+      <p class="mt-2 text-sm text-gray-500">{overview.substr(0, 280)}</p>
     </div>
   </div>
 </div>
--- src/features/Movies/MovieList.svelte
+++ src/features/Movies/MovieList.svelte
@@ -12,19 +12,21 @@
 
 <main>
   {#if $movies.length}
-    <ul transition:fly={{ y: -20, duration: 400 }}>
+    <ul
+      class="m-0 xl:grid xl:grid-cols-2"
+      transition:fly={{ y: -20, duration: 400 }}>
       {#each $movies as movie (movie.id)}
         <li
           on:click={setCurrent(movie.id)}
           class:bg-red-100={current === movie.id}
           class:shadow-lg={current === movie.id}
-          class="p-1 my-3 bg-white rounded-lg shadow cursor-pointer">
+          class="p-1 mx-0 my-3 bg-white shadow cursor-pointer xl:mx-3 md:rounded-lg xl:justify-between">
           <MovieCard {movie} />
         </li>
       {/each}
     </ul>
   {:else if $movieStore.isSearch}
-    <h3 class="text-center py-6 bg-gray-50 rounded-lg shadow mt-3">
+    <h3 class="py-6 mt-3 text-center rounded-lg shadow bg-gray-50">
       Nenhum filme encontrado
     </h3>
   {/if}
